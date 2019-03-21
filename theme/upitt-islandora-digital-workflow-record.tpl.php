104e9f939ef3f28bbe19a9aee7a5988832943c22
Willow Gillingham <bgilling@pitt.edu>
2019-03-21 17:40:57 -0400

    ISLANDORA-246 bugfix to address broken utf-8 values, save batch, transaction, and batch_item records

--- theme/upitt-islandora-digital-workflow-record.tpl.php
+++ theme/upitt-islandora-digital-workflow-record.tpl.php
@@ -1,10 +1,10 @@
 <div class="<?php print $class; ?>">
     <h3><?php print $title; ?></h3>
 <?php foreach ($row as $key => $value): ?>
-    <b><?php print $key; ?>:</b> <?php print $value; ?><br />
+    <b><?php print $key; ?>:</b> <?php print (is_null($value) ? '<i>NULL</i>' : $value); ?><br />
 <?php endforeach; ?>
-  <?php if ($extra_content): ?>
-    <?php print $extra_content; ?>
-    <br class="clearfloat" />
-  <?php endif; ?>
 </div>
+<?php if ($extra_content): ?>
+  <?php print $extra_content; ?>
+  <br class="clearfloat" />
+<?php endif; ?>
